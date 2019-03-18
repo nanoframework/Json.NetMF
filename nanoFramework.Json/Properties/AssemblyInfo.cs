4c6559f4c844e57f1faf5c3b1708fa5bf19b0d45
José Simões <jose.simoes@eclo.solutions>
2019-03-18 10:15:55 +0000

    Work on nanoFramework project
    - Rework properties.
    - Add Nerdbank.GitVersioning NuGet.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ nanoFramework.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nanoFramework.Json")]
+[assembly: AssemblyDescription("JSON Serializer and Deserializer library for nanoFramework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyProduct("Json.nanoFramework")]
+[assembly: AssemblyCopyright("Copyright © nanoFramework Contributors 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
